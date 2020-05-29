5d55bf8c3d42116c558dffd80f3f24b35700c32b
xuxiuquan <386906422@qq.com>
2020-05-29 23:06:42 +0800

    前端使用服务器地址

--- mongo/mongo-ui/src/utils/global.js
+++ mongo/mongo-ui/src/utils/global.js
@@ -1,7 +1,7 @@
 //后台管理系统服务器地址
-export const baseUrl = 'http://localhost:8001'
+export const baseUrl = 'http://172.17.96.220:8001'
 //系统数据备份还原服务器地址
-export const backupBaseUrl = 'http://localhost:8002'
+export const backupBaseUrl = 'http://172.17.96.220:8002'
 
 export default {
     baseUrl,backupBaseUrl
