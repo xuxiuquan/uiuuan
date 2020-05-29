769ac3c56e49e336fc7a6d03757194b33b0ad14b
xuxiuquan <386906422@qq.com>
2020-05-29 23:29:38 +0800

    测试csvode触发hook

--- mongo/mongo-ui/src/utils/global.js
+++ mongo/mongo-ui/src/utils/global.js
@@ -1,5 +1,5 @@
 //后台管理系统服务器地址
-export const baseUrl = 'http://172.17.96.220:8001'
+export const baseUrl = 'http://39.96.68.138:8001'
 //系统数据备份还原服务器地址
 export const backupBaseUrl = 'http://172.17.96.220:8002'
 
